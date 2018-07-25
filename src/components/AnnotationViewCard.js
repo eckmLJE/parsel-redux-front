aa5428d702f773e6726b0d253e98ad1457bd1c42
eckmLJE <lucas.eckman@gmail.com>
2018-07-25 13:31:23 -0400

    some work on annotation view card format

--- src/components/AnnotationViewCard.js
+++ src/components/AnnotationViewCard.js
@@ -5,6 +5,49 @@ import { setHoverHighlight } from "../actions/highlight";
 const card = "home-card annotation-view-card";
 
 class AnnotationViewCard extends Component {
+  state = { expanded: false, comments: false };
+
+  handleClick = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+
+  renderMinCard = () => {
+    return <div>{this.props.annotation.content.slice(0, 20) + "..."}</div>;
+  };
+
+  renderExpandCard = () => {
+    return (
+      <div className="annotation-view-expanded">
+        <div className="annotation-view-top-left">User </div>
+        <div className="annotation-view-top-right">Points </div>
+        <div className="annotation-view-body">
+          {this.props.annotation.content}
+        </div>
+        <div className="annotation-view-tags">Tags</div>
+        <div
+          className="annotation-view-bottom"
+          onClick={() => this.setState({ comments: !this.state.comments })}
+        >
+          Comments
+        </div>
+      </div>
+    );
+  };
+
+  renderCommentCard = () => {
+    return (
+      <div className="annotation-view-expanded">
+        <div className="annotation-view-top-left">User </div>
+        <div className="annotation-view-top-right">Points </div>
+        <div className="annotation-view-body">
+          {this.props.annotation.content}
+        </div>
+        <div className="annotation-view-tags">Tags</div>
+        <div className="annotation-view-bottom">Comments</div>
+      </div>
+    );
+  };
+
   render() {
     return (
       <div
@@ -13,10 +56,19 @@ class AnnotationViewCard extends Component {
             ? card + " annotation-view-card-highlight"
             : card
         }
-        onMouseEnter={() => this.props.setHoverHighlight(this.props.annotation.id)}
+        onMouseEnter={() =>
+          this.props.setHoverHighlight(this.props.annotation.id)
+        }
         onMouseLeave={() => this.props.setHoverHighlight("none")}
+        onClick={this.handleClick}
       >
-        {this.props.annotation.content}
+        {this.state.expanded && !this.state.comments
+          ? this.renderExpandCard()
+          : null}
+        {this.state.expanded && this.state.comments
+          ? this.renderCommentCard()
+          : null}
+        {!this.state.expanded ? this.renderMinCard() : null}
       </div>
     );
   }
